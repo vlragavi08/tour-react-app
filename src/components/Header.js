e0f6c4accf4f85f7ed37b7d1f4aa6fab3d92d001
vlragavi08 <ragavivl03@gmail.com>
2024-03-14 12:20:17 +0530

    title change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Header = ()=>{
 return(
  <div className='container'>
-    <div className='title'>Plan a Tour</div>
+    <div className='title'>IRCTC Tours</div>
  </div>
 )
 };
